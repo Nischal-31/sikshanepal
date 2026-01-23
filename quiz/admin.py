85f1d5b01f6af331c0fa3e97cd798560fd341f03
Nischal Moktan <nischal123321@gmail.com>
2026-01-23 21:30:10 +0545

    commited

--- quiz/admin.py
+++ quiz/admin.py
@@ -14,6 +14,36 @@ class QuizAdmin(admin.ModelAdmin):
     list_display = ('title', 'subject', 'total_marks', 'time_limit', 'is_active', 'is_paid')
     list_filter = ('subject', 'is_active', 'is_paid')
 
+class QuizAttemptAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'user',
+        'quiz',
+        'score',
+        'submitted_at',
+    )
+
+    list_filter = (
+        'quiz',
+        'submitted_at',
+    )
+
+    search_fields = (
+        'user__username',
+        'user__email',
+        'quiz__title',
+    )
+
+    ordering = (
+        '-submitted_at',
+    )
+
+    readonly_fields = (
+        'submitted_at',
+    )
+
+    list_per_page = 25
+
 admin.site.register(Quiz, QuizAdmin)
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(QuizAttempt)
+admin.site.register(QuizAttempt,QuizAttemptAdmin)
